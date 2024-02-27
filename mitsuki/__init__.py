3e83b74027febcb47a8a3d9e3606e01cfc54f061
Mifuyu <mifuyutsuki@proton.me>
2024-02-27 23:58:24 +0700

    feat: enable logging

--- mitsuki/__init__.py
+++ mitsuki/__init__.py
@@ -13,18 +13,32 @@
 from dotenv import load_dotenv
 load_dotenv(override=True)
 
+import logging
+logging.basicConfig(
+  format="%(asctime)s [%(levelname)s] %(name)s:%(lineno)d: %(message)s",
+  level=logging.INFO
+)
+
 from interactions import (
   Status,
   Activity,
   ActivityType,
   Client,
   Intents,
-  listen
+  listen,
+  BaseContext,
+  InteractionContext,
+  ComponentContext
 )
-from interactions.api.events import CommandError
+from interactions.api.events import (
+  Startup,
+  Ready,
+  CommandError,
+  CommandCompletion,
+  ComponentCompletion
+)
 from interactions.client.errors import CommandCheckFailure
 from os import environ
-import traceback
 
 from mitsuki.messages import load as load_messages
 from mitsuki.messages import message
@@ -36,6 +50,8 @@ __all__ = (
   "run",
 )
 
+logger = logging.getLogger(__name__)
+
 
 class Bot(Client):
   def __init__(self):
@@ -46,30 +62,15 @@ class Bot(Client):
         type = ActivityType.PLAYING
       )
     )
-
     self.intents  = Intents.DEFAULT
 
 
-  @listen()
+  @listen(Startup)
   async def on_startup(self):
     await initialize()
-  
-
-  @listen(CommandError, disable_default_listeners=True)
-  async def on_command_error(self, event: CommandError):
-    if isinstance(event.error, CommandCheckFailure):
-      embed = message("error_command_perms", user=event.ctx.user)
-      await event.ctx.send(embed=embed)
-      return
-    
-    traceback.print_exception(event.error)
-
-    data  = dict(error_repr=repr(event.error))
-    embed = message("error", format=data, user=event.ctx.user)
-    await event.ctx.send(embed=embed)
 
 
-  @listen()
+  @listen(Ready)
   async def on_ready(self):
     await self.change_presence(
       status=Status.ONLINE,
@@ -78,8 +79,48 @@ class Bot(Client):
         type=ActivityType.PLAYING
       )
     )
+    logger.info(f"Ready: {self.user.tag} ({self.user.id})")
+    
 
-    print(f"Ready: {self.user.tag} ({self.user.id})")
+  @listen(CommandError, disable_default_listeners=True)
+  async def on_command_error(self, event: CommandError):
+    if isinstance(event.error, CommandCheckFailure):
+      embed = message("error_command_perms", user=event.ctx.user)
+      await event.ctx.send(embed=embed)
+      return
+    
+    logger.exception(event.error)
+
+    data  = dict(error_repr=repr(event.error))
+    embed = message("error", format=data, user=event.ctx.user)
+    await event.ctx.send(embed=embed)
+  
+
+  @listen(CommandCompletion)
+  async def on_command_completion(self, event: CommandCompletion):
+    if isinstance(event.ctx, InteractionContext):
+      command_name = event.ctx.invoke_target
+      logger.info(f"Command emitted: {command_name}")
+      if len(event.ctx.args) > 0:
+        args = [str(v) for v in event.ctx.args]
+        logger.info(f"args: {args}")
+      if len(event.ctx.kwargs) > 0:
+        kwargs = {k: str(v) for k, v in event.ctx.kwargs.items()}
+        logger.info(f"kwargs: {kwargs}")
+  
+
+  @listen(ComponentCompletion)
+  async def on_component_completion(self, event: ComponentCompletion):
+    command_name = event.ctx.invoke_target
+    try:
+      component_name = event.ctx.custom_id.split("|")[1]
+    except Exception:
+      component_name = event.ctx.custom_id
+    
+    logger.info(f"Component emitted: {command_name}: {component_name}")
+    if len(event.ctx.values) > 0:
+      values = [str(v) for v in event.ctx.args]
+      logger.info(f"values: {values}")
 
 
 print(f"Mitsuki {__version__}")
