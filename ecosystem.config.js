29bdf6aff03a59937006facc1f42b034848fb835
Mifuyu <mifuyutsuki@proton.me>
2024-03-28 02:06:12 +0700

    fix: add extended kill timeout

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,7 @@ module.exports = {
     name          : "mitsuki",
     script        : "./run.py",
     interpreter   : "./.venv/bin/python",
-    restart_delay : 30000
+    restart_delay : 30000,
+    kill_timeout  : 5000
   }]
 }
