5efa86414208d2423e4a9c0644d83e0a48c3bb41
Mifuyu <mifuyutsuki@proton.me>
2025-08-19 22:46:29 +0700

    feat: submitter for updating gacha roster

---
+++ mitsuki/core/submitter/base.py
@@ -0,0 +1,66 @@
+# Copyright (c) 2024-2025 Mifuyu (mifuyutsuki@proton.me)
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+from typing import Optional, Any, Union, Self
+from datetime import timedelta
+from enum import IntEnum
+import csv
+import yaml
+import uuid
+
+import attrs
+import interactions as ipy
+import sqlalchemy as sa
+from sqlalchemy import select, update, delete, literal
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from mitsuki.utils import option, ratio, process_text
+from mitsuki.lib.userdata import begin_session, AsDict, sa_insert as insert
+from mitsuki.lib.commands import CustomID
+from mitsuki.core.settings import get_setting, Settings
+
+
+_entries: dict[uuid.UUID, "BaseSubmitter"] = {}
+
+
+@attrs.define(kw_only=True)
+class BaseSubmitter:
+  id: uuid.UUID = attrs.field(factory=uuid.uuid4)
+  data: dict = attrs.field(factory=dict)
+  timeout: float = attrs.field(default=300)
+  expires: ipy.Timestamp = attrs.field(factory=ipy.Timestamp.now)
+
+
+  def __attrs_post_init__(self):
+    self.expires = ipy.Timestamp.now() + timedelta(seconds=self.timeout)
+
+
+  @classmethod
+  def fetch(cls, id: Union[uuid.UUID, str]) -> Optional[Self]:
+    global _entries
+    now = ipy.Timestamp.now()
+
+    if isinstance(id, str):
+      id = uuid.UUID(id)
+
+    # Cache expiration
+    # Global instance may be altered, so we make a copy of keys()
+    for _id in list(_entries.keys()):
+      if _entries[_id].expires < now:
+        _entries.pop(_id)
+
+    return _entries.get(id)
+
+
+  def save(self):
+    global _entries
+    _entries[self.id] = self
---
+++ tests/gacha/test_submitter.py
@@ -0,0 +1,97 @@
+import pytest
+import pytest_asyncio
+
+import os
+import asyncio
+from datetime import datetime, timedelta, timezone
+
+import sqlalchemy as sa
+import interactions as ipy
+import attrs
+import yaml
+
+import mitsuki
+
+from mitsuki import settings, utils
+from mitsuki.lib import checks
+from mitsuki.lib.userdata import begin_session
+
+from mitsuki.core import gacha
+from mitsuki.core.submitter import CardSubmitter, CardSubmitterErrors
+
+
+async def test_card_submitter_create(init_db, card_rarities: list[gacha.CardRarity], card_data: dict):
+  submitter = await CardSubmitter.from_rosc2y_yaml(card_data)
+
+  assert submitter.add_count == len(card_data)
+  assert submitter.edit_count == 0
+  assert submitter.delist_count == 0
+  assert submitter.original_count == 0
+  assert sum(submitter.error_counts.values()) == 0
+
+
+async def test_card_submitter_create_missing_rarity(init_db, card_data: dict):
+  submitter = await CardSubmitter.from_rosc2y_yaml(card_data)
+
+  assert submitter.add_count == 0
+  assert submitter.edit_count == 0
+  assert submitter.delist_count == 0
+  assert submitter.original_count == 0
+  assert submitter.error_counts[CardSubmitterErrors.RARITY] == len(card_data)
+
+
+async def test_card_submitter_fetch(init_db, card_rarities: list[gacha.CardRarity], card_data: dict):
+  created = await CardSubmitter.from_rosc2y_yaml(card_data)
+  fetched = CardSubmitter.fetch(created.id)
+
+  assert fetched == created
+
+
+async def test_roster_add(init_db, cards: list[gacha.Card], card_data: dict):
+  other_card_data = {
+    "c00.01.1": {"name": "Mitsuki", "rarity": 1, "type": "Character", "series": "Mitsuki"}
+  }
+  submitter = await CardSubmitter.from_rosc2y_yaml(card_data | other_card_data)
+
+  assert submitter.add_count == len(other_card_data)
+  assert submitter.edit_count == 0
+  assert submitter.delist_count == 0
+  assert submitter.original_count == len(cards)
+  assert sum(submitter.error_counts.values()) == 0
+
+  fetched_submitter = CardSubmitter.fetch(submitter.id)
+  assert fetched_submitter == submitter
+
+
+async def test_roster_edit(init_db, cards: list[gacha.Card], card_data: dict):
+  edited_card_data = {}
+  for id, entry in card_data.items():
+    entry["name"] = entry["name"] + " (Swimsuit ver.)"
+    edited_card_data[id] = entry
+
+  submitter = await CardSubmitter.from_rosc2y_yaml(edited_card_data)
+
+  assert submitter.add_count == 0
+  assert submitter.edit_count == len(edited_card_data)
+  assert submitter.delist_count == 0
+  assert submitter.original_count == len(cards)
+  assert sum(submitter.error_counts.values()) == 0
+
+  fetched_submitter = CardSubmitter.fetch(submitter.id)
+  assert fetched_submitter == submitter
+
+
+async def test_roster_overwrite(init_db, cards: list[gacha.Card], card_data: dict):
+  other_card_data = {
+    "c00.01.1": {"name": "Mitsuki", "rarity": 1, "type": "Character", "series": "Mitsuki"}
+  }
+  submitter = await CardSubmitter.from_rosc2y_yaml(other_card_data)
+
+  assert submitter.add_count == len(other_card_data)
+  assert submitter.edit_count == 0
+  assert submitter.delist_count == len(cards)
+  assert submitter.original_count == len(cards)
+  assert sum(submitter.error_counts.values()) == 0
+
+  fetched_submitter = CardSubmitter.fetch(submitter.id)
+  assert fetched_submitter == submitter
