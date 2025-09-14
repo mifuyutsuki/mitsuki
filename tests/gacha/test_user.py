c0242ae4cb31dc2d12fefc7d29aab7918e7fd38d
Mifuyu <mifuyutsuki@proton.me>
2025-09-14 20:29:58 +0700

    test(gacha): add tests for dailies with different daily resets

--- tests/gacha/test_user.py
+++ tests/gacha/test_user.py
@@ -16,7 +16,38 @@ from mitsuki import settings, utils
 from mitsuki.lib import checks
 from mitsuki.lib.userdata import begin_session
 
+from mitsuki.core.settings import get_setting, set_setting, Settings
 from mitsuki.core import gacha
+
+TEST_DATETIME = ipy.Timestamp(
+  year=2024, month=2, day=14, hour=0, minute=15, second=0, microsecond=0, tzinfo=timezone.utc
+)
+"""Reference time for tests, corresponding to 14 Feb 2024 00:15:00."""
+
+# TEST_DATETIME_ALT = ipy.Timestamp(year=2024, month=2, day=14, hour=4, minute=15, second=0, microsecond=0)
+# """Alternative reference time for tests, corresponding to 14 Feb 2024 00:15:00."""
+
+
+@pytest.fixture(params=[
+  "00:00",
+  "04:00",
+  "21:00",
+])
+async def custom_daily_reset(request: pytest.FixtureRequest):
+  async with begin_session() as session:
+    await set_setting(session, Settings.DailyResetTime, request.param)
+
+  daily_reset_s = await get_setting(Settings.DailyResetTime)
+  h, m = daily_reset_s.split(":")
+  return TEST_DATETIME.replace(hour=int(h), minute=int(m))
+
+
+@pytest.fixture()
+async def gacha_user_custom_reset(mock_user, custom_daily_reset: ipy.Timestamp):
+  # Similar to gacha_user, but the daily reset is parametrized
+  # Set to be daily reset - 1h
+  async with begin_session() as session:
+    return await gacha.GachaUser.daily(session, mock_user.id, now=custom_daily_reset - timedelta(hours=1))
 
 
 async def test_fetch_user(init_db, mock_user, gacha_user: gacha.GachaUser):
@@ -53,11 +84,19 @@ async def test_fetch_profile(init_db, mock_user, card_rolls: list[gacha.Card], g
   assert created.claimed_daily
 
 
-async def test_daily_unclaimed(init_db, mock_user, gacha_user: gacha.GachaUser):
-  # last user daily (now) < last reset < current time (now + 1d)
-  now = ipy.Timestamp.now() + timedelta(days=1)
+async def test_daily_unclaimed(
+  init_db, mock_user, custom_daily_reset: ipy.Timestamp, gacha_user_custom_reset: gacha.GachaUser
+):
+  # last user daily < last reset < "current" time
 
-  user_before = gacha_user
+  # Daily reset | Last daily | "Current" time
+  # 14/2 00:00  | 13/2 23:00 | 14/2 06:00
+  # 14/2 04:00  | 14/2 03:00 | 14/2 10:00
+  # 14/2 21:00  | 14/2 20:00 | 15/2 03:00
+
+  now = custom_daily_reset + timedelta(hours=1)
+
+  user_before = gacha_user_custom_reset
   async with begin_session() as session:
     user_after = await gacha.GachaUser.daily(session, mock_user.id, now=now)
 
@@ -66,11 +105,19 @@ async def test_daily_unclaimed(init_db, mock_user, gacha_user: gacha.GachaUser):
   assert not user_after.claimed_first_daily
 
 
-async def test_daily_claimed(init_db, mock_user, gacha_user: gacha.GachaUser):
-  # last reset < current time (now - 1d) < last user daily (now)
-  now = ipy.Timestamp.now() - timedelta(days=1)
+async def test_daily_claimed(
+  init_db, mock_user, custom_daily_reset: ipy.Timestamp, gacha_user_custom_reset: gacha.GachaUser
+):
+  # last user daily < "current" time < last reset
 
-  user_before = gacha_user
+  # Daily reset | Last daily | "Current" time
+  # 14/2 00:00  | 13/2 23:00 | 13/2 23:30
+  # 14/2 04:00  | 14/2 03:00 | 14/2 03:30
+  # 14/2 21:00  | 14/2 20:00 | 14/2 20:30
+
+  now = custom_daily_reset - timedelta(minutes=30)
+
+  user_before = gacha_user_custom_reset
   async with begin_session() as session:
     user_after = await gacha.GachaUser.daily(session, mock_user.id, now=now)
 
@@ -79,13 +126,15 @@ async def test_daily_claimed(init_db, mock_user, gacha_user: gacha.GachaUser):
   assert not user_after.claimed_first_daily
 
 
-async def test_daily_burst(init_db, mock_user, gacha_user: gacha.GachaUser):
+async def test_daily_burst(
+  init_db, mock_user, custom_daily_reset: ipy.Timestamp, gacha_user_custom_reset: gacha.GachaUser
+):
   # last reset < current time (now - 1d) < last user daily (now)
   user_before = await gacha.GachaUser.fetch(mock_user.id)
   assert user_before is not None
 
   # First call
-  now = ipy.Timestamp.now() + timedelta(days=1, minutes=1)
+  now = custom_daily_reset + timedelta(days=1, minutes=1)
   async with begin_session() as session:
     user_after = await gacha.GachaUser.daily(session, mock_user.id, now=now)
 
